bd2e92c79704d8a4859ddafa6f3794b25b8229d7
spdev7 <sauravprakash7@outlook.com>
2019-03-31 16:33:55 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Header  from './Header'
+import Header from './Header'
 import Hello from './Hello';
-import { Route , Switch,BrowserRouter as Router } from 'react-router-dom'
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
 import './style.css';
 import Card from './Card'
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faSearch,faHeart,faUser,faHome} from '@fortawesome/free-solid-svg-icons';
-library.add(faSearch,faUser,faHeart,faHome);
+import { faSearch, faHeart, faUser, faHome } from '@fortawesome/free-solid-svg-icons';
+library.add(faSearch, faUser, faHeart, faHome);
 import Profile from './Profile';
 
 
@@ -22,12 +22,11 @@ class App extends Component {
   render() {
     return (
       <div>
-      <Router>
-        <Route path='/' component={Header} />
-        <Route path='/Profile'  exact component={Profile} />
-        <Route path='/' exact component={Card} />
-        
-       </Router>
+        <Router>
+          <Route path='/' component={Header} />
+          <Route path='/Profile' exact component={Profile} />
+          <Route path='/' exact component={Card} />
+        </Router>
       </div>
     );
   }
